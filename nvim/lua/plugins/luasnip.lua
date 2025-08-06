84a127627be2f2fb87a071e44e7644219199d885
daonm <daobilionusd@gmail.com>
2025-08-06 16:45:06 +0700

    Remove unused files

--- nvim/lua/plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "L3MON4D3/LuaSnip",
-    event = "InsertEnter",
-    config = function()
-      require("luasnip.loaders.from_lua").load({ paths = { vim.fn.stdpath("config") .. "/lua/snippets/" } })
-    end,
-  },
-}
